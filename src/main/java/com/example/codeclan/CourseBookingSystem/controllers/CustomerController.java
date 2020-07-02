ababb8a8db17a75caf39a35df5469809a6a5c488
Nathan <nathankirley@gmail.com>
2020-07-02 12:25:25 +0100

    getCustomer in customer controller

--- src/main/java/com/example/codeclan/CourseBookingSystem/controllers/CustomerController.java
+++ src/main/java/com/example/codeclan/CourseBookingSystem/controllers/CustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +35,10 @@ public class CustomerController {
             return new ResponseEntity<>(customerRepository.findByBookingsCourse(course), HttpStatus.OK);
         }
         return new ResponseEntity<>(customerRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/customers/{id}")
+    public ResponseEntity getCustomer(@PathVariable Long id){
+        return new ResponseEntity(customerRepository.findById(id), HttpStatus.OK);
     }
 }
