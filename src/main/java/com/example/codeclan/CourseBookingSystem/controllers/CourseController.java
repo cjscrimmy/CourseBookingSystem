007f8e0fbd5add75acb8476962f7dc69909dbcef
Nathan <nathankirley@gmail.com>
2020-07-02 14:25:32 +0100

    postCourse in course controller

--- src/main/java/com/example/codeclan/CourseBookingSystem/controllers/CourseController.java
+++ src/main/java/com/example/codeclan/CourseBookingSystem/controllers/CourseController.java
@@ -7,10 +7,7 @@ import com.example.codeclan.CourseBookingSystem.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -40,5 +37,11 @@ public class CourseController {
     @GetMapping(value = "/courses/{id}")
     public ResponseEntity getCourse(@PathVariable Long id){
         return new ResponseEntity(courseRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/courses")
+    public ResponseEntity<Course> postCourse(@RequestBody Course course){
+        courseRepository.save(course);
+        return new ResponseEntity<>(course, HttpStatus.CREATED);
     }
 }
