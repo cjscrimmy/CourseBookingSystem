3eac82ce4dea2ff20af3f1269dde31a5970f1bb6
Nathan <nathankirley@gmail.com>
2020-07-02 12:14:47 +0100

    findCustomersByCourseOrName customer controller

--- src/main/java/com/example/codeclan/CourseBookingSystem/controllers/CustomerController.java
+++ src/main/java/com/example/codeclan/CourseBookingSystem/controllers/CustomerController.java
@@ -16,23 +16,23 @@ import java.util.List;
 @RestController
 public class CustomerController {
 
-//    @Autowired
-//    CustomerRepository customerRepository;
-//
-//    @Autowired
-//    CourseRepository courseRepository;
-//
-//    @GetMapping(value = "/customers")
-//    public ResponseEntity<List<Customer>> findCustomersByCourseOrName(
-//            @RequestParam(name = "course", required = false) String courseName,
-//            @RequestParam(name = "name", required = false) String customerName){
-//        if (courseName != null){
-//            Course course = courseRepository.findByName(courseName);
-//            return new ResponseEntity<>(customerRepository.findByBookingsCourse(course), HttpStatus.OK);
-//        }
-//        if (customerName != null){
-//            return new ResponseEntity<>(customerRepository.findByName(customerName), HttpStatus.OK);
-//        }
-//        return new ResponseEntity<>(customerRepository.findAll(), HttpStatus.OK);
-//    }
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @GetMapping(value = "/customers")
+    public ResponseEntity<List<Customer>> findCustomersByCourseOrName(
+            @RequestParam(name = "customer", required = false) String customerName,
+            @RequestParam(name = "course", required = false) String courseName){
+        if (customerName != null){
+            return new ResponseEntity(customerRepository.findByName(customerName), HttpStatus.OK);
+        }
+        if (courseName != null){
+            Course course = courseRepository.findByName(courseName);
+            return new ResponseEntity<>(customerRepository.findByBookingsCourse(course), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(customerRepository.findAll(), HttpStatus.OK);
+    }
 }
