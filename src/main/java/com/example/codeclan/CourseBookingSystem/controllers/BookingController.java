07631e1648693af72a9f386b54696873c5f0b598
Nathan <nathankirley@gmail.com>
2020-07-02 14:18:12 +0100

    postBooking in booking controller

--- src/main/java/com/example/codeclan/CourseBookingSystem/controllers/BookingController.java
+++ src/main/java/com/example/codeclan/CourseBookingSystem/controllers/BookingController.java
@@ -5,10 +5,7 @@ import com.example.codeclan.CourseBookingSystem.repositories.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,5 +27,11 @@ public class BookingController {
     @GetMapping(value = "/bookings/{id}")
     public ResponseEntity getCustomer(@PathVariable Long id){
         return new ResponseEntity(bookingRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/bookings")
+    public ResponseEntity<Booking> postBooking(@RequestBody Booking booking){
+        bookingRepository.save(booking);
+        return new ResponseEntity<>(booking, HttpStatus.CREATED);
     }
 }
